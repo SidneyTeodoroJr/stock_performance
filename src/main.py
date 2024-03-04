1898122f1fe461bf1efdb484a88b161bce5ae6f7
Sidney Teodoro Araujo Araujo <sidneyteodoro2002@gmail.com>
2024-03-04 15:33:38 -0300

    comit

--- src/main.py
+++ src/main.py
@@ -1,24 +1,35 @@
-# Importando dependências
+# Importando as bibliotecas necessárias
 import streamlit as st
 import yfinance as yf
 import pandas as pd
 
 st.title("FINANCE DASHBOARD")
+ 
+tickers = ("TSLA", "AAPL", "MSFT", "BTC-USD", "ETH-USD", "NVDA") # Lista de tickers
 
-tickers = ("TSLA", "AAPL", "MSFT", "BTC-USD", "ETH-USD", "NVDA")
+dropdown = st.multiselect("Pick your assets", tickers) # Menu
 
-dropdown = st.multiselect("Pick your assets", tickers)
+# Campos de entrada 
+start = st.date_input("Start", value=pd.to_datetime("2020-01-01"))
+end = st.date_input("End", value=pd.to_datetime("today"))
 
-start = st.date_input("Start", value= pd.to_datetime("2020-01-01"))
-end = st.date_input("End", value = pd.to_datetime("today"))
-
+# Calcular os retornos relativos dos ativos 
 def relativeret(df):
-    rel = df.pct_change()
-    cumret = (1 + rel).cumprod() - 1
-    cumret = cumret.fillna(0)
+    rel = df.pct_change()               
+    cumret = (1 + rel).cumprod() - 1    
+    cumret = cumret.fillna(0)           
     return cumret
 
+
+# Verifica se foi selecionado
 if len(dropdown) > 0:
-    df = relativeret(yf.download(dropdown, start, end)["Adj Close"])
-    st.header("Returns of {}".format(dropdown))
+    df = relativeret(yf.download(dropdown, start, end)["Adj Close"]) # Baixa os dados 
+    
+    st.header("Returns of: {}".format(dropdown)) # Cabeçalho do gráfico
+    
+    data_frame = pd.DataFrame(df)
+    st.dataframe(data_frame, use_container_width=True)
+
     st.line_chart(df)
+
+    st.bar_chart(df)
